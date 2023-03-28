30af76d41a26bfbd957b7ec5a060b45f8b21db92
Sergii Sadovyi <1175636+sergsadovyi@users.noreply.github.com>
2023-03-28 16:35:33 +0200

    feat: use bash for scripting

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM node:18-alpine
 
+RUN \
+    apk add --no-cache bash
+
 COPY run.sh /run.sh
 
 ENTRYPOINT ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,27 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 function main() {
   PACKAGE_FULL_NAME=$(node -p "require('./package.json').name")
+  echo "📦 Working on package: $PACKAGE_FULL_NAME"
+
   IFS='/' read -ra ADDR <<< "$PACKAGE_FULL_NAME"
   PACKAGE_SCOPE=${ADDR[0]}
   PACKAGE_NAME=${ADDR[1]}
+  echo "📦 Package scope: $PACKAGE_SCOPE"
+  echo "📦 Package name: $PACKAGE_NAME"
 
   REGISTRY_VERSION=$(npm view $PACKAGE_FULL_NAME version)
   LOCAL_VERSION=$(node -p "require('./package.json').version")
+  echo "📦 Local version: $LOCAL_VERSION"
+  echo "📦 Registry version: $REGISTRY_VERSION"
 
   if [ "$REGISTRY_VERSION" = "$LOCAL_VERSION" ]; then
-    echo "Package version is already published"
+    echo "🚧 Package version is already published"
     exit 0
   fi
 
+  echo "🔑 NPM Registry: Login to ${INPUT_REGISTRY}"
   echo "${PACKAGE_SCOPE}:registry=https://${INPUT_REGISTRY}" >> .npmrc && \
   echo "//${INPUT_REGISTRY}/:_authToken=${NPM_AUTH_TOKEN}" >> .npmrc && \
 
+  echo "👷🛠️ Build package"
   npm ci
   npm run build
 
+  echo "👷⬆️ Push to registry"
   if [ "$INPUT_ACCESS" = "public" ]; then
     npm publish --access public
   else
